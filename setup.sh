8edacc8ba0e694a04b47849a5a8aa9060cdf11f5
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2020-10-28 09:52:28 +0530

    Update and rename README.md to setup.sh

---
+++ setup.sh
@@ -0,0 +1,46 @@
+RED='\033[01;31m'
+echo -e "${RED} Updating and upgrading"
+echo -e "\e[0m"
+pkg update && pkg upgrade
+pkg install figlet
+echo -e "${RED} Bash Code By DSB"
+figlet RYU CODER
+YELLOW='\033[01;33m'                                         
+CYAN='\033[01;36m'                                           
+echo -e "        ${CYAN}This is bash scrpting"
+echo -e "This script will install all dependences for notebook"
+echo-e "${RED}instaling clang for c/c++"
+echo -e "\e[0m"
+apt install clang
+echo -e "${RED} installing python "
+echo -e "\e[0m"
+apt install  python
+echo -e "${RED}installing fftw"
+echo -e "\e[0m"
+apt install fftw 
+echo -e "${RED}installing main lib"
+echo -e "\e[0m"
+apt install libzmq 
+echo -e "${RED}installing main freetype"
+apt install freetype
+echo -e "${RED}Upgarding pip to latest verson"
+echo -e "\e[0m"
+pip install --upgrade pip
+echo -e "${RED}Installing pkg config "
+echo -e "\e[0m"
+apt install pkg-config
+echo -e "${RED}installing png libray"
+echo -e "\e[0m"
+apt install libpng
+echo -e "${RED}Setting up env"
+echo -e "\e[0m"
+LDFLAGS="-lm -lcompiler_rt" 
+echo -e "${RED}installing jupyter notebook"
+echo -e "\e[0m"
+pip install jupyter 
+echo -e "${RED}Testing notebook"
+echo -e "${YELLOW}If notebook launched sucess you can see some thing like this ${CYAN}To access the notebook, openlike this file in a browser:"
+echo -e "\e[0m"
+cls
+jupyter notebook
+    
